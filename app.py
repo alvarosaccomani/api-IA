bb7edecf2bd142373a650ad4d76d4e71f88bfbe8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-09-23 09:50:31 -0300

    i add the app.py and health.py files to project

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from routes.embeddings import embeddings_bp
+from routes.health import health_bp
+from routes.open_router import open_router_bp
+
+app = Flask(__name__)
+
+# Registrar los blueprints
+app.register_blueprint(health_bp, url_prefix="/health")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
